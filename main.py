9ba5964521a4ffd6b0bb124f4a1e16c46d8e0a34
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-09 09:57:29 +0000

    Add player class

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1. Set up players
+# 2. set up board
+# 3. Get move from player
+# 4. Check for winner
+# 5. Switch player
+
+from player import Player
+
+def round():
+    pass
+
+if __name__ == "__main__":
+    # Init players
+    player1 = Player('Dan', 'X')
+    player2 = Player('John', 'O')
+    print(player1, player2)
---
+++ player.py
@@ -0,0 +1,4 @@
+class Player:
+    def __init__(self, name, mark):
+        self.name = name
+        self.mark = mark
