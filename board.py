3bffe95ea3f2322a41ddd37e1585d126ac31c740
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-12 21:31:03 +0000

    remove unwanted comments and add formatting

--- board.py
+++ board.py
@@ -1,15 +1,19 @@
 class Board:
     def __init__(self):
-        self.board = [['   ', '   ', '   '], ['   ', '   ', '   '], ['   ', '   ', '   ']]
+        self.board = [
+            ["   ", "   ", "   "],
+            ["   ", "   ", "   "],
+            ["   ", "   ", "   "],
+        ]
 
     def show(self):
         print()
-        top_row = [' A ', ' B ', ' C ']
+        top_row = [" A ", " B ", " C "]
         print(" ", " ".join(top_row))
         for row in enumerate(self.board):
-            print(row[0] + 1, '|'.join(row[1]))
-            if row[0] < len(self.board) -1:
-                print('  -----------')
+            print(row[0] + 1, "|".join(row[1]))
+            if row[0] < len(self.board) - 1:
+                print("  -----------")
         print()
 
     def check_win(self):
@@ -31,7 +35,10 @@ class Board:
 
         # Diagonals
         all_cells = [item for lst in self.board for item in lst]
-        diagonals = [[all_cells[0], all_cells[4], all_cells[8]],[all_cells[2], all_cells[4], all_cells[6]]]
+        diagonals = [
+            [all_cells[0], all_cells[4], all_cells[8]],
+            [all_cells[2], all_cells[4], all_cells[6]],
+        ]
         for diagonal in diagonals:
             if "   " in diagonal:
                 continue
@@ -40,29 +47,26 @@ class Board:
         # Top left to bottom right
         # if all_cells[0] == all_cells[4] and all_cells[4] == all_cells[8]:
         #     return "win"
-        
+
         # # Top right to bottom left
         # if all_cells[2] == all_cells[4] and all_cells[4] == all_cells[6]:
         #     return "win"
 
         # no spaces left
         if not spaces_left:
-            return 'draw'
+            return "draw"
         return False
-        
 
-    
     def move(self, player, coordinates):
         print(coordinates)
-        if self.board[coordinates[0]][coordinates[1]] == '   ':
-            self.board[coordinates[0]][coordinates[1]] = f' {player.mark} '
+        if self.board[coordinates[0]][coordinates[1]] == "   ":
+            self.board[coordinates[0]][coordinates[1]] = f" {player.mark} "
             print(coordinates)
             return True
         else:
             return False
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     board = Board()
     board.show()
--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-# 1. Set up players
-# 2. set up board
-# 3. Get move from player
-# 4. Check for winner
-# 5. Switch player
-
 from player import Player
 from board import Board
 from random import choice
 import re
 
-columns = {
-    'a': 0,
-    'b': 1,
-    'c': 2
-}
+columns = {"a": 0, "b": 1, "c": 2}
+
 
 def round():
     starting_player = choice((player1, player2))
     current_player = starting_player
-    print(f'Player {starting_player.name} will go first')
+    print(f"Player {starting_player.name} will go first")
     board = Board()
     board.show()
     while True:
         try:
-            player_move = input('Please enter the coordinates of the space you want to mark. e.g. 1C would be row 1, column 3 (top right): ')
+            player_move = input(
+                "Please enter the coordinates of the space you want to mark. e.g. 1C would be row 1, column 3 (top right): "
+            )
             coords = []
-            coords_pattern = re.compile('[1-3][a-c]')
+            coords_pattern = re.compile("[1-3][a-c]")
             if coords_pattern.match(player_move.lower()):
-                coords.append(int(player_move[0])-1)
+                coords.append(int(player_move[0]) - 1)
                 coords.append(columns[player_move[1].lower()])
                 print(coords)
 
@@ -35,43 +28,46 @@ def round():
                 print("Wrong amount of numbers!")
                 continue
             if not board.move(current_player, coords):
-                print('That spot is already taken!')
+                print("That spot is already taken!")
                 continue
         except (IndexError, ValueError):
-            print('Please make sure you entered the coordinates correctly')
+            print("Please make sure you entered the coordinates correctly")
             continue
         board.show()
         result = board.check_win()
-        if result == 'win':
+        if result == "win":
             current_player.wins += 1
-            print(f'The winner is {current_player.name}!')
+            print(f"The winner is {current_player.name}!")
             break
-        if result == 'draw':
-            print('It\'s a draw!')
+        if result == "draw":
+            print("It's a draw!")
             break
-        if current_player.mark == 'X':
+        if current_player.mark == "X":
             current_player = player2
         else:
             current_player = player1
         print(f"\nIt's {current_player.name}'s turn")
 
+
 def game(number_of_rounds):
     for i in range(number_of_rounds):
-        print('\nROUND', i+1)
+        print("\nROUND", i + 1)
         round()
-        print(f'The scores at the end of this round are:\n{player1.name}: {player1.wins}\n{player2.name}: {player2.wins}')
-        
+        print(
+            f"The scores at the end of this round are:\n{player1.name}: {player1.wins}\n{player2.name}: {player2.wins}"
+        )
+
 
 if __name__ == "__main__":
     # Init players
-    print('Welcome to TIC-TAC-TOE')
+    print("Welcome to TIC-TAC-TOE")
     player1_name = input("Enter the name of the first player: ")
     player2_name = input("Enter the name of the second player: ")
     if player1_name == "":
-        player1_name = 'X'
+        player1_name = "X"
     if player2_name == "":
-        player2_name = 'O'
-    player1 = Player(player1_name.upper(), 'X')
-    player2 = Player(player2_name.upper(), 'O')
-    print(f'Round 1: {player1.name} VS {player2.name}')
+        player2_name = "O"
+    player1 = Player(player1_name.upper(), "X")
+    player2 = Player(player2_name.upper(), "O")
+    print(f"Round 1: {player1.name} VS {player2.name}")
     game(3)
